aefb9d5480a77289bf2d87b7db54d3c9c616904d
luca-nik <luca.nicoli.engineer@gmail.com>
2026-02-02 14:32:46 +0100

    Add rewrite rule to proxy /docs to GitBook

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: 'https://lucas-documentation.gitbook.io/filautocap-docs',
+      },
+      {
+        source: '/docs/:path*',
+        destination: 'https://lucas-documentation.gitbook.io/filautocap-docs/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
